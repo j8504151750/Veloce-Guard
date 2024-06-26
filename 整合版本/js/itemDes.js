a288ae7e44764ea1b98aff360de2adcbdcf803c0
BernieCHC <b1235698741@gmail.com>
2024-06-26 16:37:44 +0800

    itemDes edited

--- 整合版本/js/itemDes.js
+++ 整合版本/js/itemDes.js
@@ -1,33 +1,26 @@
 $(document).ready(function() {
-    // Replace with the actual endpoint URL that returns product details in JSON format
-    var apiEndpoint = 'http://yourdomain.com/api/product/details';
 
-    // Making an AJAX request to the API endpoint
     $.ajax({
-        url: apiEndpoint,
+        url: 'http://localhost:8080/api/public/1/variants?pageNumber=0&pageSize=2&sortBy=productId&sortOrder=asc',
         type: 'GET',
-        dataType: 'json', // Expecting JSON data in response
+        dataType: 'json', 
+        headers: {
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJVc2VyIERldGFpbHMiLCJpc3MiOiJFdmVudCBTY2hlZHVsZXIiLCJpYXQiOjE3MTkzNzAxMDksImVtYWlsIjoiam9obi5kb2VAZXhhbXBsZS5jb20ifQ.ZtDAkSxwjs0knMxF-1GOEutn_4KmB_176TmFks8rrWo'
+        },
         success: function(data) {
-            // Assuming the API returns data in the format:
-            // {
-            //   "name": "Nylon Fleece Hooded Sweater",
-            //   "description": "A new style inspired by...",
-            //   "price": "6980",
-            //   "imageSrc": "./images/MENS TOPS/top1.jpg"
-            // }
-
-            // Dynamically update the HTML with data from the API
             $('.image-sell').html('<img src="' + data.imageSrc + '" alt="' + data.name + '">');
             $('.product-introduction h2').text(data.name);
             $('.product-introduction p:first').text(data.description);
             $('.product-introduction p:last').text('價格: $ ' + data.price);
         },
         error: function(err) {
-            // Handle any errors
-            console.log("Error fetching data: ", err);
-            // Optionally, update the HTML to show an error message
-            $('.image-sell').html('<p>Error loading image.</p>');
-            $('.product-introduction').html('<p>Error loading product details.</p>');
+ 
+
+            console.log(err);
+            // $('.image-sell').html('<img src="' + err.productVariants.image +'">');
+            // $('.product-introduction h2').text(product.name);
+            // $('.product-introduction p:first').text(product.description);
+            // $('.product-introduction p:last').text('價格: $ ' + product.price);
         }
     });
 });
