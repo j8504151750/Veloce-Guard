2e147a13c0a56cfe83c33ccaa37df980ff5afaeb
BernieCHC <b1235698741@gmail.com>
2024-06-30 17:08:27 +0800

    product.html的JS更新成新版/稍微美化商品頁

--- 整合版本/js/product-scrip-NEW.js
+++ 整合版本/js/product-scrip-NEW.js
@@ -1,24 +1,25 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $.ajax({
         url: 'http://localhost:8080/api/products',
         type: 'GET',
         contentType: 'application/json',
-        success: function(response) {
-            response.forEach(function(product) {
+        success: function (response) {
+            response.forEach(function (product) {
                 var firstVariantImage = product.productVariants.length > 0 ? product.productVariants[0].image : 'defaultImagePath.jpg';
                 $('#productContainer').append(
-                    `<div class="product col-4 float-left" style="cursor: pointer;">
-                        <a href="itemDesJT.html?productId=${product.productId}">
-                            <div class="image-container">
-                                <img src="${firstVariantImage}" alt="${product.name}" />
-                                <div>${product.name}</div>
-                            </div>
-                        </a>
+                    `<div class="product col-4 float-left">
+                    <div class="image-container">
+                        <img src="${firstVariantImage}" alt="${product.name}" />
+                    </div>
+                    
+                    <div>${product.name}</div>
+                    <a href="itemDesJT.html?productId=${product.productId}" class="btn btn-secondary" style="width:100%; height:50px; margin-bottom:50px;">點我看詳細</a>
+                        <br>
                     </div>`
                 );
             });
         },
-        error: function(error) {
+        error: function (error) {
             console.log("Failed to load products:", error);
         }
     });
